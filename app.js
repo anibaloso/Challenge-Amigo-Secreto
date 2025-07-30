431eafdd5e80f4478b571b0773ab7472d25cad06
Anibaloso <anibaloso@live.com>
2025-07-29 23:13:38 -0400

    Creacion función Agregar Amigos

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let nombres=[];
+
+//Se implementa funcion para agregar amigos al apretar el boton
+function agregarAmigo(){
+    let nombre=document.getElementById('amigo').value;;
+    if(nombre){
+        nombres.push(nombre);
+        console.log(nombres)
+        document.querySelector('#amigo').value='';
+    }else{
+        alert('Por favor, inserte un nombre.');
+    }
+}
+
