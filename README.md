0c8b412be99a477be0e0710adc2f86bb68b4d9fd
anibal <anibaloso@live.com>
2025-07-30 15:57:29 -0400

    Creacion README.md

---
+++ README.md
@@ -0,0 +1,20 @@
+<em> # Challenge Amigo Secreto</em>
+
+Este proyecto es una aplicación web interactiva para organizar un sorteo de amigo secreto. Permite a los usuarios agregar nombres de participantes y realizar un sorteo aleatorio para asignar un amigo secreto a cada persona.
+
+## 🚀 Funcionalidades
+
+- **Agregar nombres**: Los usuarios pueden ingresar los nombres de los participantes.
+- **Listar participantes**: Los nombres ingresados se muestran en una lista.
+- **Sortear amigo secreto**: Se selecciona aleatoriamente un participante como amigo secreto.
+
+## 🛠️ Tecnologías utilizadas
+
+- **HTML**: Estructura del contenido.
+- **CSS**: Estilos y diseño visual.
+- **JavaScript**: Lógica de la aplicación.
+
+## Autores
+
+| [<img src="https://github.com/anibaloso.png" width=115><br><sub>Luis Anibal Rodriguez</sub>](https://github.com/anibaloso) 
+| :-------------------------------------------------------------------------------------------------------------------------------------------------: |
--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let nombres=[];
+let nombres = [];
 
 //Se implementa funcion para agregar amigos al apretar el boton
-function agregarAmigo(){
-    let nombre=document.getElementById('amigo').value;;
-    if(nombre){
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;;
+    if (nombre) {
         nombres.push(nombre);
         console.log(nombres);
-        document.querySelector('#amigo').value='';
+        document.querySelector('#amigo').value = '';
         listarAmigos();
-    }else{
+    } else {
         alert('Por favor, inserte un nombre.');
     }
 }
 
 // Crea una función que recorra el array amigos y agregue cada nombre como un elemento <li> dentro de una lista HTML. Usa innerHTML para limpiar la lista antes de agregar nuevos elementos.
 
-function listarAmigos(){
-    let listaAmigos=document.getElementById('listaAmigos');
-    listaAmigos.innerHTML='';
-    nombres.forEach(nombre=>{
-        const li=document.createElement('li');
-        li.textContent=nombre;
+function listarAmigos() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+    nombres.forEach(nombre => {
+        const li = document.createElement('li');
+        li.textContent = nombre;
         listaAmigos.appendChild(li);
     })
 }
 
 // Escribe una función que seleccione de manera aleatoria uno de los nombres almacenados en el array amigos. Usa Math.random() y Math.floor() para obtener un índice aleatorio.
 
-function sortearAmigo(){
-    let amigoElegido=Math.floor(Math.random() * nombres.length);
+function sortearAmigo() {
+    let amigoElegido = Math.floor(Math.random() * nombres.length);
     console.log("🚀 ~ sortearAmigo ~ amigoElegido:", amigoElegido)
-    
+
     alert(`El amigo elegido es : ${nombres[amigoElegido]}`);
 }
